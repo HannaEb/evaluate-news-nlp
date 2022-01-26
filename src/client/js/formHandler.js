13c2f720dda440ab81a1915738568cb0a23c252c
Hanna <hannaeberhoefer@yahoo.com>
2022-01-26 14:24:22 +0000

    refactor: handleSubmit and postData

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,28 +1,27 @@
 function handleSubmit(event) {
-    event.preventDefault()
+  event.preventDefault()
 
-    let formText = document.getElementById('text').value;
+  let formText = document.getElementById('text').value;
+  const data = {
+      'txt': formText
+  }
 
-    const newLink = `http://localhost:8081/add?ff=${formText}`;
-    Client.checkForText(formText)
-
-    postData(newLink)
-    .then((data) => {
-        console.log(data);
-    })
+  if (Client.checkForText(formText)) {
+      postData(data)
+  }
 }
 
-const postData = async (url= '') => {
-    const res = await fetch(url, {
+const postData = async (data) => {
+    const res = await fetch('http://localhost:8081/postData', {
         method: 'POST', 
         credentials: 'same-origin',
         headers: {
             'Content-Type': 'application/json',
-        }
+        },
+        body: JSON.stringify(data)
     })
     try {
         const newData = await res.json();
-        console.log(newData);
         document.getElementById('agreement').innerHTML = `${newData.agreement}`
         document.getElementById('subjectivity').innerHTML = `${newData.subjectivity}`
         document.getElementById('confidence').innerHTML = `${newData.confidence}`
